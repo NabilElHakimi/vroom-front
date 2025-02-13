873a5ca141669f13d3eb752efc84469e405d8094
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2025-02-13 14:48:15 +0100

    add header, footer, and carousel components; integrate Flowbite for UI enhancements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './client/components/header/header.component';
+import {CarouselHomeComponent} from './client/components/carousel-home/carousel-home.component';
+import {FooterComponent} from './client/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CarouselHomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
