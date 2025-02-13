9966826a7457f14371344eedec0682b202804268
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2025-02-13 15:18:16 +0100

    add vehicle card component and integrate it into the home page layout

--- src/app/client/pages/home-page/home-page.component.ts
+++ src/app/client/pages/home-page/home-page.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {FooterComponent} from '../../components/footer/footer.component';
 import {CarouselHomeComponent} from '../../components/carousel-home/carousel-home.component';
 import {HeaderComponent} from '../../components/header/header.component';
+import {VehicleCardComponent} from '../../components/vehicle-card/vehicle-card.component';
 
 @Component({
   selector: 'app-home-page',
   imports: [
     FooterComponent,
     CarouselHomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    VehicleCardComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
