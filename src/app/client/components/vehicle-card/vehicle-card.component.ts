622e7eb6ad3734f39d0feac590784373f072cb33
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2025-02-14 21:42:50 +0100

    feat: add isNewVehicle method to vehicle card component and display "Nouveau" label for new vehicles

--- src/app/client/components/vehicle-card/vehicle-card.component.ts
+++ src/app/client/components/vehicle-card/vehicle-card.component.ts
@@ -1,14 +1,30 @@
 import {Component, Input} from '@angular/core';
 import {Vehicle} from '../../../model/Vehicle';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-vehicle-card',
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './vehicle-card.component.html',
   styleUrl: './vehicle-card.component.css'
 })
 export class VehicleCardComponent {
 
   @Input() vehicle!: Vehicle;
+
+  isNewVehicle(): boolean {
+    if (!this.vehicle || !this.vehicle.createdAt) {
+      return false;
+    }
+
+    const createdAtDate = new Date(this.vehicle.createdAt);
+    const sevenDaysAgo = new Date();
+    sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+
+    return createdAtDate >= sevenDaysAgo;
+  }
+
 
 }
