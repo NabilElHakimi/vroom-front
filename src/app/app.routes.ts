ac912012c1158e3ecda94bacb073029ef0e54274
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2025-02-16 12:11:55 +0100

    feat: add registration component with routing and form template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomePageComponent } from './client/pages/home-page/home-page.component'
 import { PageClientComponent } from './client/pages/page-client/page-client.component';
 import {DetailsPageComponent} from './client/pages/details-page/details-page.component';
 import {LoginComponent} from './auth/login/login.component';
+import {RegisterComponent} from './auth/register/register.component';
 
 export const routes: Routes = [
   {
@@ -30,6 +31,11 @@ export const routes: Routes = [
   {
     path : 'login',
     component : LoginComponent
+  },
+  {
+    path : 'register',
+    component : RegisterComponent
   }
+
 
 ];
