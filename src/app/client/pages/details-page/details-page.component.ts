d6532c407f7283974409f025ab2888baf4fff79c
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2025-02-18 21:15:02 +0100

    feat: implement vehicle retrieval and display in details page

--- src/app/client/pages/details-page/details-page.component.ts
+++ src/app/client/pages/details-page/details-page.component.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {VehicleDetailsComponent} from '../../components/vehicle-details/vehicle-details.component';
 import {VehicleCardComponent} from '../../components/vehicle-card/vehicle-card.component';
+import {NgForOf} from '@angular/common';
+import {Vehicle} from '../../../model/Vehicle';
+import {SuccesstoastService} from '../../../services/toast-service/successtoast.service';
+import {VehicleService} from '../../../services/vehicle-service/vehicle.service';
 
 @Component({
   selector: 'app-details-page',
   imports: [
     VehicleDetailsComponent,
-    VehicleCardComponent
+    VehicleCardComponent,
+    NgForOf
   ],
   templateUrl: './details-page.component.html',
   styleUrl: './details-page.component.css'
 })
-export class DetailsPageComponent {
+export class DetailsPageComponent implements OnInit {
+
+  constructor(private vehicleService : VehicleService ,
+              private successToastService : SuccesstoastService) {
+
+  }
+
+  vehicles: Vehicle[] = [] ;
+
+  ngOnInit(): void {
+    this.getVehicle();
+  }
+
+  getVehicle(): void {
+    this.vehicleService.getVehicle().subscribe((data: any) => {
+      this.vehicles = data.content;
+
+      // this.successToast = true;
+      setTimeout(() => {
+        // this.successToast = false;
+        // this.message = '';
+      }, 5000);
+    });
+  }
 
 }
