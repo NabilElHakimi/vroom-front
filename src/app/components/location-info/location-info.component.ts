89df97329feeab14142897174e0dda13786b0ad5
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2025-03-02 16:31:33 +0000

    feat: add location info component and integrate it into the location page

---
+++ src/app/components/location-info/location-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-info',
+  imports: [],
+  templateUrl: './location-info.component.html',
+  styleUrl: './location-info.component.css'
+})
+export class LocationInfoComponent {
+
+}
--- src/app/pages/location-page/location-page.component.ts
+++ src/app/pages/location-page/location-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {LocationInfoComponent} from '../../components/location-info/location-info.component';
 
 @Component({
   selector: 'app-location-page',
-  imports: [],
+  imports: [
+    LocationInfoComponent
+  ],
   templateUrl: './location-page.component.html',
   styleUrl: './location-page.component.css'
 })
